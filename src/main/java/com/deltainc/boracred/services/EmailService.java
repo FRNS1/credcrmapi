5e94ea64e68457a51472e235aad9727dfd202b30
FRNS1 <fernandesjoao2009@hotmail.com>
2023-09-21 16:34:37 -0300

    make it better

--- src/main/java/com/deltainc/boracred/services/EmailService.java
+++ src/main/java/com/deltainc/boracred/services/EmailService.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-import com.deltainc.boracred.htmltemplates
-
 @Service
 public class EmailService {
 
