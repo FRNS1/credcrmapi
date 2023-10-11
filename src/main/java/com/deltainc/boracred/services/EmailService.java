96f026d8521435fca3d86297661f3851e7dc9659
Matheus Fernandes <matheus.fernandes@delta.nt>
2023-10-11 11:15:37 -0300

    double check aprovacao

--- src/main/java/com/deltainc/boracred/services/EmailService.java
+++ src/main/java/com/deltainc/boracred/services/EmailService.java
@@ -26,6 +26,17 @@ public class EmailService {
         sendHtmlEmail(to, subject, htmlContent);
     }
 
+    public void sendEmailPreAprovado(String to, String nomeCliente, Integer idProposta, Float taxa, Float valorLiberado, Integer prazo) throws Exception{
+        String subject = String.format("O cliente %s, aceitou a proposta %d", nomeCliente, idProposta);
+        String htmlContent = EmailTemplates.templatePreAprovado;
+        htmlContent = htmlContent.replace("${nomeCliente}", nomeCliente);
+        htmlContent = htmlContent.replace("${idProposta}", idProposta.toString());
+        htmlContent = htmlContent.replace("${taxa}", taxa.toString());
+        htmlContent = htmlContent.replace("${valorLiberado}", valorLiberado.toString());
+        htmlContent = htmlContent.replace("${prazo}", prazo.toString());
+        sendHtmlEmail(to, subject, htmlContent);
+    }
+
     public void sendEmailNovaProposta(String to, String nomeCliente, Integer idProposta, Float valorDesejado, Integer prazo) throws Exception{
         String subject = String.format("Nova proposta do cliente %s.", nomeCliente, idProposta);
         String htmlContent = EmailTemplates.templateNovaProposta;
